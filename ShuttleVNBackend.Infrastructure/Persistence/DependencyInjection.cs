004e2ad849c88be9b22188c6195876db202bb085
vietng <vietng322611@gmail.com>
2026-09-05 12:42:38 +0700

    add infrastructure DI

---
+++ ShuttleVNBackend.Infrastructure/Persistence/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ShuttleVNBackend.Infrastructure.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ShuttleVnDbContext>(options =>
+            options.UseNpgsql(
+                configuration.GetConnectionString("DefaultConnection")));
+
+        return services;
+    }
+}
