dc458be6fff13875da1b52df4f4e4c83ef87d4d5
vietng <vietng322611@gmail.com>
2026-09-07 09:36:20 +0700

    move repository interfaces to application layer; refactor `IUserAccountRepository`

---
+++ ShuttleVNBackend.Application/Interfaces/Repositories/IUserAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShuttleVNBackend.Application.Common;
+using ShuttleVNBackend.Core.Entities.User;
+
+namespace ShuttleVNBackend.Application.Interfaces.Repositories;
+
+public interface IUserAccountRepository
+{
+    Task<List<UserAccount>> GetAllAsync(PageRequest page, CancellationToken ct = default);
+    Task<UserAccount?> GetByIdAsync(Guid id, CancellationToken ct = default);
+    Task<UserAccount?> GetByUsernameOrEmailAsync(string username, CancellationToken ct = default);
+    Task<UserAccount> AddAsync(UserAccount userAccount, CancellationToken ct = default);
+    Task SoftDeleteAsync(Guid id, CancellationToken ct = default);
+}
