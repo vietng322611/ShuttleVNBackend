0068571084678006c02d175830741fd2caa31cf1
vietng <vietng322611@gmail.com>
2026-09-09 20:11:45 +0700

    fix typo

--- ShuttleVNBackend.Application/DTOs/User/CustomerProfileDto.cs
+++ ShuttleVNBackend.Application/DTOs/User/CustomerProfileDto.cs
@@ -4,5 +4,5 @@ public record CustomerProfileDto
 {
     public string FullName { get; set; } = string.Empty;
     public string Phone { get; set; } = string.Empty;
-    public string Emai { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
 }
