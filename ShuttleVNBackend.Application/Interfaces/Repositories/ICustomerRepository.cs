14868af461499edb17af80a3bb19d26210987fcb
vietng <vietng322611@gmail.com>
2026-09-09 13:14:14 +0700

    add `CustomerRepository` and implementation

---
+++ ShuttleVNBackend.Application/Interfaces/Repositories/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using ShuttleVNBackend.Application.Common;
+using ShuttleVNBackend.Core.Entities.User;
+
+namespace ShuttleVNBackend.Application.Interfaces.Repositories;
+
+public interface ICustomerRepository
+{
+    public Task<List<Customer>> GetAllCustomers(PageRequest page, CancellationToken ct = default);
+    public Task<Customer?> GetCustomerById(Guid id, CancellationToken ct = default);
+    public Task<Customer?> GetCustomerByEmail(string email, CancellationToken ct = default);
+}
---
+++ ShuttleVNBackend.Infrastructure/Persistence/Repositories/CustomerRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShuttleVNBackend.Application.Common;
+using ShuttleVNBackend.Application.Interfaces.Repositories;
+using ShuttleVNBackend.Core.Entities.User;
+
+namespace ShuttleVNBackend.Infrastructure.Persistence.Repositories;
+
+public class CustomerRepository(ShuttleVnDbContext dbContext): ICustomerRepository
+{
+    public async Task<List<Customer>> GetAllCustomers(PageRequest page, CancellationToken ct = default)
+    {
+        var skip = (page.PageNumber - 1) * page.PageSize;
+        return await dbContext.Customers
+            .OrderBy(x => x.CreatedAt)
+            .Skip(skip)
+            .Take(page.PageSize)
+            .ToListAsync(ct);
+    }
+
+    public async Task<Customer?> GetCustomerById(Guid id, CancellationToken ct = default)
+        => await dbContext.Customers.FirstOrDefaultAsync(
+            c => c.CustomerId == id, ct);
+
+    public async Task<Customer?> GetCustomerByEmail(string email, CancellationToken ct = default)
+        => await dbContext.Customers
+            .FirstOrDefaultAsync(c => c.Email == email, ct);
+}
